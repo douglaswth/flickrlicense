ebb5177ebc97ca1b3b04db36f993eb2a28a364b2
Douglas Thrift <douglas.thrift@rightscale.com>
2017-11-22 19:02:23 -0800

    Populate photos

--- app.rb
+++ app.rb
@@ -28,27 +28,58 @@ migration 'create users, licenses, and photos tables' do
 
   database.create_table :photos do
     column :id, Integer, primary_key: true
+    # foreign_key :user_id, :users
     foreign_key :owner, :users
+    # foreign_key :license_id, :licenses
     foreign_key :license, :licenses
     column :json, 'text'
     column :ignore, 'boolean'
   end
 end
 
+migration 'rename photo owner and license columns to user_id and license_id' do
+  database.alter_table :photos do
+    drop_foreign_key [:owner]
+    rename_column :owner, :user_id
+    add_foreign_key [:user_id], :users
+    drop_foreign_key [:license]
+    rename_column :license, :license_id
+    add_foreign_key [:license_id], :licenses
+  end
+end
+
 class User < Sequel::Model
-  one_to_many :photo, key: :owner
+  one_to_many :photo
   unrestrict_primary_key
 end
 
 class License < Sequel::Model
-  one_to_many :photo, key: :license
+  one_to_many :photo
   unrestrict_primary_key
 end
 
 class Photo < Sequel::Model
-  many_to_one :user, key: :owner
-  many_to_one :license, key: :license
+  many_to_one :user
+  many_to_one :license
   unrestrict_primary_key
+
+  def flickraw
+    @flickraw ||= OpenStruct.new(JSON.parse(json))
+  end
+
+  def as_json(*)
+    {
+      id: id,
+      license: license_id,
+      ignore: ignore,
+      image: FlickRaw.url_q(flickraw),
+      link: FlickRaw.url_photopage(flickraw),
+    }
+  end
+
+  def to_json(*args)
+    as_json.to_json(*args)
+  end
 end
 
 helpers Flickr::Login::Helpers
@@ -88,6 +119,20 @@ end
 
 get %r{/photos/([1-8])} do |page|
   page, per_page = page.to_i, 500
-  photos = flickr.photos.search(user_id: :me, extras: 'license', per_page: per_page, page: page)
-  json page: page, per_page: per_page, photos: photos
+  photos = Photo.reverse(:id).limit(per_page, (page - 1) * per_page).all
+  begin
+    photos = flickr.photos.search(user_id: :me, extras: 'license', per_page: per_page, page: page).map do |flickr_photo|
+      Photo.create do |photo|
+        photo.id = flickr_photo.id
+        photo.user_id = flickr_photo.owner
+        photo.license_id = flickr_photo.license
+        photo.json = flickr_photo.to_hash.to_json
+        photo.ignore = false
+      end
+    end if photos.count == 0
+  rescue Sequel::UniqueConstraintViolation
+    # sometimes the Flickr API will just keep repeating the same results for subsequent pages
+  end
+  json path: "/photos/#{page + 1}", photos: photos if photos.count == per_page && page < 8
+  json photos: photos
 end
